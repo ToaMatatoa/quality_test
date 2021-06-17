eef3060fd0771ea26d0d7744a03234e345193612
Matatoa <rikonikotako@gmail.com>
2021-06-17 14:54:37 +0300

    first commit

---
+++ app/src/main/java/com/example/qualitytest/AllCurrenciesFragment.kt
@@ -0,0 +1,6 @@
+package com.example.qualitytest
+
+import androidx.fragment.app.Fragment
+
+class AllCurrenciesFragment : Fragment(R.layout.fragment_all_currencies) {
+}
---
+++ app/src/main/java/com/example/qualitytest/MyCurrenciesFragment.kt
@@ -0,0 +1,6 @@
+package com.example.qualitytest
+
+import androidx.fragment.app.Fragment
+
+class MyCurrenciesFragment : Fragment(R.layout.fragment_my_currencies) {
+}
