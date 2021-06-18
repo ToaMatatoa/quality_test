8e7e290ab0f56f682ad9ed414bae02a5fad4e35e
Matatoa <rikonikotako@gmail.com>
2021-06-18 17:59:35 +0300

    changed activity_main.xml, bottom_nav_menu.xml, MainActivity.kt added nav_graph.xml
    Added navigation graph

--- app/src/main/java/com/example/qualitytest/MainActivity.kt
+++ app/src/main/java/com/example/qualitytest/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.qualitytest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.qualitytest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,25 +19,33 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        val firstFragment = AllCurrenciesFragment()
-        val secondFragment = MyCurrenciesFragment()
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.findNavController()
 
-        setCurrentFragment(firstFragment)
+        binding.bottomNavigationView.setupWithNavController(navController)
 
-        binding.bottomNavigationView.setOnNavigationItemSelectedListener {
-            when (it.itemId) {
-                R.id.home -> setCurrentFragment(firstFragment)
-                R.id.person -> setCurrentFragment(secondFragment)
 
-            }
-            true
-        }
-
+//        val firstFragment = AllCurrenciesFragment()
+//        val secondFragment = MyCurrenciesFragment()
+//
+//        setCurrentFragment(firstFragment)
+//
+//        binding.bottomNavigationView.setOnNavigationItemSelectedListener {
+//            when (it.itemId) {
+//                R.id.all_currencies -> setCurrentFragment(firstFragment)
+//                R.id.my_currencies -> setCurrentFragment(secondFragment)
+//
+//            }
+//            true
+//        }
+//
+//    }
+//
+//    private fun setCurrentFragment(fragment: Fragment) =
+//        supportFragmentManager.beginTransaction().apply {
+//            replace(R.id.flFragment, fragment)
+//            commit()
+//        }
     }
-
-    private fun setCurrentFragment(fragment: Fragment) =
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.flFragment, fragment)
-            commit()
-        }
 }
